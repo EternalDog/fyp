958e588000a7df116d4e5f105a35039fe854249f
Dominik <31939441+EternalDog@users.noreply.github.com>
2019-02-18 00:52:46 +0000

    Update dog.js
    can send strings trough the web socket

--- static/javascript/dog.js
+++ static/javascript/dog.js
@@ -1,20 +1,18 @@
-console.log("dog");
-
 //const url = 'wss://flavio-websockets-server-example.glitch.me';
 //const url = 'wss://8080';
-const url = 'wss://localhost:8080';
-
+const url = 'ws://localhost:8080';
 const connection = new WebSocket(url);
 
 connection.onopen = () => {
-  connection.send('hey') 
+  connection.send('hey');
   console.log("open");
 }
 
 connection.onerror = (error) => {
-  console.log(`WebSocket error: ${error}`)
+  console.log(`WebSocket error: ${error}`);
+  console.log("err");
 }
 
 connection.onmessage = (e) => {
-  console.log(e.data)
+  console.log(e.data);
 }
