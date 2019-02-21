498dc549862a45605697cb15e7c0905d56937f1c
Dominik <31939441+EternalDog@users.noreply.github.com>
2019-02-21 13:12:59 +0000

    Add files via upload

--- static/javascript/dog.js
+++ static/javascript/dog.js
@@ -3,10 +3,11 @@
 const url = 'ws://localhost:8080';
 const connection = new WebSocket(url);
 
-let dog = {1: "dog", 2: "dog"};
+let dog = "Hello";
+let input = {str: ""};
 
 connection.onopen = () => {
-  connection.send(dog);
+  connection.send(text);
   console.log("open");
 }
 
@@ -17,4 +18,16 @@ connection.onerror = (error) => {
 
 connection.onmessage = (e) => {
   console.log(e.data);
+  document.getElementById("ret").innerHTML = e.data;
+}
+
+function send(){
+  input.str = document.getElementById("txtinput").value;
+
+  connection.send(document.getElementById("txtinput").value);
+
+  $.get("/dog", (input) , function(data, status){
+    document.getElementById("ret2").innerHTML = data;
+    console.log("Data: " + data + "\nStatus: " + status);
+  });
 }
